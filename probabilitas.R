ae1c83eacd8214be06ddd7e871e7daf1f49f674e
muhammadimam15 <72868243+muhammadimam15@users.noreply.github.com>
2020-10-14 01:39:45 -1200

    Add files via upload

---
+++ probabilitas.R
@@ -0,0 +1,5 @@
+#Kemungkinan seorang balita tidak di imunisasi campak adalah 1/5. 
+#Pada tanggal 26 Juni 2016, di klinik Anda terdapat 4 orang balita. 
+#Berapakah peluang dari balita tersebut 2 orang belum mendapatkan imunisasi campak?
+
+dbinom(x=2, size = 4, prob = 1/5)
